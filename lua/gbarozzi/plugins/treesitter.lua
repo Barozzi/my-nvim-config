47b59bbae9ec2324a96c1a6491b9a6503d5d0b4b
Greg Barozzi <gb0218@dexcom.com>
2025-03-28 12:24:41 -0700

    add mdx syntax highlighting

--- lua/gbarozzi/plugins/treesitter.lua
+++ lua/gbarozzi/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  event = { 'BufReadPre', 'BufNewFile' },
+  build = ':TSUpdate',
   dependencies = {
     -- import treesitter plugin
-    "windwp/nvim-ts-autotag",
+    'windwp/nvim-ts-autotag',
   },
   config = function()
-    local treesitter = require("nvim-treesitter.configs")
+    local treesitter = require 'nvim-treesitter.configs'
 
-    treesitter.setup({
+    treesitter.setup {
       highlight = {
         enable = true,
       },
@@ -20,33 +20,33 @@ return {
         enable = true,
       },
       ensure_installed = {
-        "glimmer",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "json",
-        "html",
-        "css",
-        "svelte",
-        "markdown",
-        "go",
-        "vim",
-        "dockerfile",
-        "lua",
-        "bash",
-        "ruby",
-        "scala",
+        'glimmer',
+        'javascript',
+        'typescript',
+        'tsx',
+        'yaml',
+        'json',
+        'html',
+        'css',
+        'svelte',
+        'markdown',
+        'go',
+        'vim',
+        'dockerfile',
+        'lua',
+        'bash',
+        'ruby',
+        'scala',
       },
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
           scope_incremental = false,
-          node_decremental = "<bs>",
+          node_decremental = '<bs>',
         },
       },
-    })
+    }
   end,
 }
